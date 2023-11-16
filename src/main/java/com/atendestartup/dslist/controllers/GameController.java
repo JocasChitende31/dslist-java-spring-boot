4b2c12e4c7cc26e98a53aff1c199a410062e45db
JocasChitende31 <84420296+JocasChitende31@users.noreply.github.com>
2023-11-16 17:56:52 +0100

    Get Game By Id

--- src/main/java/com/atendestartup/dslist/controllers/GameController.java
+++ src/main/java/com/atendestartup/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.atendestartup.dslist.dto.GameDTO;
 import com.atendestartup.dslist.dto.GameMinDTO;
 import com.atendestartup.dslist.services.GamesServices;
 
@@ -17,14 +19,18 @@ public class GameController {
 
 //	Inject a GameServices component to the GameController component
 	@Autowired
-	private GamesServices gameServices;
+	private GamesServices gameService;
+
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
 
 //	making a get request mapping to a GameServices where there is a logicBusisses of get data
 	@GetMapping
 	public List<GameMinDTO> findAll() {
-
-		List<GameMinDTO> result = gameServices.findAll();
-		
+		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
 
--- src/main/java/com/atendestartup/dslist/dto/GameDTO.java
+++ src/main/java/com/atendestartup/dslist/dto/GameDTO.java
@@ -1,53 +1,36 @@
-package com.atendestartup.dslist.entities;
+package com.atendestartup.dslist.dto;
 
-import java.util.Objects;
+import org.springframework.beans.BeanUtils;
+
+import com.atendestartup.dslist.entities.Game;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "tb_game")
-public class Game {
+public class GameDTO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
-
-	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-
 	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-
 	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
-	public Game() {
+	public GameDTO() {
 
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
-			String shortDescription, String longDesription) {
-		this.id = id;
-		this.title = title;
-		this.year = year;
-		this.genre = genre;
-		this.platforms = platforms;
-		this.score = score;
-		this.imgUrl = imgUrl;
-		this.shortDescription = shortDescription;
-		this.longDescription = longDesription;
+	public GameDTO(Game entity) {
+//		This library make the same things as usual, but in the sort codes
+		BeanUtils.copyProperties(entity, this);
 	}
 
+//	Because of using The above library at the constructor we must provider 
+//	getMethods but also set;
 	public Long getId() {
 		return id;
 	}
@@ -116,25 +99,7 @@ public class Game {
 		return longDescription;
 	}
 
-	public void setLongDescription(String longDesription) {
-		this.longDescription = longDesription;
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Game other = (Game) obj;
-		return Objects.equals(id, other.id);
-	}
-
 }
